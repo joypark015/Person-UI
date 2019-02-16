d70a0e11b3214e46acc7841cba34baf17225b959
Park <jcpfmc@ENGR-PARKCHANW1.local>
2019-02-15 21:49:51 -0600

    First commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Park, Joy (MU-Student) on 2/15/19.
+//  Copyright © 2019 Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
